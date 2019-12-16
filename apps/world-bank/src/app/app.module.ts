2fd613eafe97a118b60ac8873a3cabefa55216e8
Ricardo Manso Martín <ricardo.manso@goldensoft.local>
2019-12-16 16:34:17 +0100

    Instalación y configuración NgRX 0.1.5

--- apps/world-bank/src/app/app.module.ts
+++ apps/world-bank/src/app/app.module.ts
@@ -12,6 +12,14 @@ import { ROUTES } from './app.routes';
 import { PaisesService } from './services/paises.service';
 import { RegionComponent } from './components/region/region.component';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromApp from './+state/app.reducer';
+import { AppEffects } from './+state/app.effects';
+import { NxModule } from '@nrwl/angular';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule, RouterState, routerReducer } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -25,11 +33,26 @@ import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot( ROUTES, { useHash: true} )
+    RouterModule.forRoot(ROUTES, { useHash: true }),
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      {
+        router: routerReducer
+      },
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot({ routerState: RouterState.Minimal }),
+    StoreModule.forFeature(fromApp.APP_FEATURE_KEY, fromApp.reducer)
   ],
-  providers: [
-    PaisesService
-  ],
+  providers: [PaisesService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
