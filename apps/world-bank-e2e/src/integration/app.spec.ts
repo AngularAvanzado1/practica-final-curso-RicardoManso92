1294d2219b6251bda4a2273542a445886c11330c
Vanessa ortega quevedo <mansomata@gmail.com>
2019-12-22 19:27:41 +0100

    0.1.9 Mejora test e2e

--- apps/world-bank-e2e/src/integration/app.spec.ts
+++ apps/world-bank-e2e/src/integration/app.spec.ts
@@ -1,10 +1,13 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting, getNumeroRegiones } from '../support/app.po';
 
 describe('world-bank', () => {
   beforeEach(() => cy.visit('/'));
   context('WHEN: user visits home page', () => {
     it('should display welcome message', () => {
       getGreeting().contains('Regiones Geográficas');
+    });
+    it('THEN: should display div regions', () => {
+      getNumeroRegiones().should('have.length', 1)
     });
   });
 });
